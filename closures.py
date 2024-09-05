cf1bdc82c6b595217124de5f955706b0340e158a
Muzi1995 <muzammil.kamran1995@gmail.com>
2024-09-05 17:35:52 +0100

    Add files via upload

---
+++ closures.py
@@ -0,0 +1,32 @@
+# A closure in Python is a technique where a nested (inner) function remembers and has access
+# to the variables and scope of its enclosing (outer) function,
+# even after the outer function has finished executing.
+
+def parent_function(person, coins):
+    # coins = 3
+
+    def play_game():
+        nonlocal coins
+        coins -= 1
+
+        if coins > 1:
+            print("\n" + person + " has " + str(coins) + " coins left.")
+
+        elif coins == 1:
+            print("\n" + person + " has " + str(coins) + " coin left.")
+
+        else:
+            print("\n" + person + " is out of coins.")
+
+    return play_game
+
+
+tommy = parent_function("Tommy", 3)
+jenny = parent_function("Jenny", 5)
+
+tommy()
+tommy()
+
+jenny()
+
+tommy()
